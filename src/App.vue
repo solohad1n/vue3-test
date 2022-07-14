72d9260a5e14a5007aaf6f3297eb5e5163a7d7a2
solohad1n <fox663399@gmail.com>
2022-07-14 12:03:28 +0600

    separation of components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <PostForm @asd="createPost" />
+  <PostList :posts="posts" />
 </template>
 
+<script>
+import PostForm from "./components/PostForm.vue";
+import PostList from "./components/PostList.vue";
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "Заголовок 1", body: "Тело нашего поста 1" },
+        { id: 2, title: "Заголовок 2", body: "Тело нашего поста 2" },
+        { id: 3, title: "Заголовок 3", body: "Тело нашего поста 3" },
+        { id: 4, title: "Заголовок 4", body: "Тело нашего поста 4" },
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+    },
+  },
+  components: { PostForm, PostList },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="form-post">
+    <form @submit.prevent>
+      <h2>Создать пост:</h2>
+      <input v-model="post.title" type="text" placeholder="Название:" />
+      <input v-model="post.body" type="text" placeholder="Описание:" />
+      <button @click="createPost">Создать</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("asd", this.post);
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-post {
+  margin: 0 0 50px 0;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+  text-align: center;
+  margin: auto;
+}
+</style>
